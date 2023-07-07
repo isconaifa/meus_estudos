a56f5867d16b5e2881fe4cc093498c479bdec2cc
isconaifagit config --global user.name isconaifagit config --global user.name isconaifa <iscobasdostdbv@gmail.com>
2023-07-07 12:47:45 -0400

    primeiro commit

---
+++ Pilotando com a DOM/modal.js
@@ -0,0 +1,6 @@
+const buttonOpenModal = document.getElementById('openModal')
+
+const modalWrapper = document.querySelector('modal-wrapper')
+buttonOpenModal.onclick = function() {
+modalWrapper.classList.remove('invisible')
+}
